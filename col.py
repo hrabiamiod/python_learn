ca7f0e6d14200ad4dedcdb1576935afe4dd6f44e
Michał W <majkel.design@gmail.com>
2019-01-06 22:01:34 +0100

    z weryfikacja int

--- col.py
+++ col.py
@@ -1,10 +1,18 @@
 print('Podaj liczbe')
-a = int(input())
+liczba = input()
 
-while a != 1:
-    if a % 2 == 0:
-        a = a // 2
-        print(a)
-    elif a % 2 == 1:
-        a = a * 3 + 1
-        print(a)
+def collatz(a):
+    while a != 1:               # Do kiedy a nie bedzie cyfra 1
+        if a % 2 == 0:          # Jesli modulo wynosi zero
+            a = a // 2          # dziel przez dwa
+            print(a)
+        elif a % 2 == 1:        # Jesli modulo wynosi jeden
+            a = a * 3 + 1       # pomnoz przez 3 i dodaj 1
+            print(a)
+
+
+try:                            # Weryfikacjia czy input jest liczba
+    b = int(liczba)
+    collatz(b)
+except ValueError:              # Jesli nie to
+    print('Liczba musi być cyfrą!')
